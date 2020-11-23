52f4910d4a80ec6b58c9d371835fa5b642638901
Silvia Vargas <silvia.vargas@studio.unibo.it>
2020-11-23 16:48:24 +0100

    modified array u in Diffusion_2D.py

--- Diffusion_2D.py
+++ Diffusion_2D.py
@@ -5,41 +5,39 @@ author: Silvia Vargas
 """
 import numpy as np
 
-def diffuse(I, nu, L, dt, F, T): 
+def diffuse(I, nu, L, F, T): #tolto dt come parametro
     import time;  t0 = time.clock()  # For measuring the CPU time
     dx,dy= L* 0.01
+    dx2, dy2 = dx*dx, dy*dy
+    dt = F * (dx2 * dy2) / (nu * (dx2 + dy2))
     Nt = int(round(T/float(dt)))
     t = np.linspace(0, Nt*dt, Nt+1)   # Mesh points in time
     Nx = int(round(L/dx))
     Ny = int(round(L/dy))
     x = np.linspace(0, L, Nx+1)       # Mesh points in space 
     y = np.linspace(0, L, Ny+1)        
-    dx2, dy2 = dx*dx, dy*dy
-    dt = F * (dx2 * dy2) / (nu * (dx2 + dy2))
+    
     # Make sure dx and dt are compatible with x and t
     dx = x[1] - x[0]
     dy = y[1] - y[0]
     dt = t[1] - t[0]
     
-    u   = np.zeros((Nx+1, Ny+1))      # solution array
-    u_n = np.zeros((Nx+1, Ny+1))      # solution at t-dt
+    u   = np.zeros((Nx+1, Ny+1, Nt+1))      # solution array     
      
 # Set initial condition
     for i in range(0,Nx+1):
-        for j in range (0,Ny+1):
-         u_n[i,j] = I(x[i],y[j])
+     for j in range (0,Ny+1):
+         u[i,j,0] = I[x[i],y[j]]#modificato da funz a array
  
 #Run through Nt timesteps    
-    for n in range(0, Nt):       
-      u[1:-1, 1:-1] = u_n[1:-1, 1:-1] + nu * dt * (
-          (u_n[2:, 1:-1] - 2*u_n[1:-1, 1:-1] + u_n[:-2, 1:-1])/dx2
-          + (u_n[1:-1, 2:] - 2*u_n[1:-1, 1:-1] + u_n[1:-1, :-2])/dy2 )
+    for n in range(1, Nt):       
+      u[1:-1, 1:-1, n] = u[1:-1, 1:-1, n-1] + nu * dt * (
+          (u[2:, 1:-1, n-1] - 2*u[1:-1, 1:-1, n-1] + u[:-2, 1:-1,n-1])/dx2
+          + (u[1:-1, 2: ,n-1] - 2*u[1:-1, 1:-1, n-1] + u[1:-1, :-2, n-1])/dy2 )
       
-# Switch variables before next step
-    u_n = u.copy()
     
     t1 = time.clock()
-    return u #t1-t0 #u_n? 
+    return u, t1-t0 #u_n? 
 
 
 
