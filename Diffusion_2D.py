60e60376ebdeb671a59ac964c275162b7625a319
Silvia Vargas <silvia.vargas@studio.unibo.it>
2020-11-30 11:28:50 +0100

    Updated Diffusion_2D.py

--- Diffusion_2D.py
+++ Diffusion_2D.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-author: Silvia Vargas
+Created on Wed Nov ‎11 ‏‎11:23:29 2020
 
+@author: Silvia Vargas
 """
-import scipy as sp
+import numpy as np
 import matplotlib.pyplot as plt
 
 class Diffusion(object):
@@ -18,14 +19,12 @@ class Diffusion(object):
                  self.nt = nt  #Number of time-steps to evolve system.
                  self.dx2 = dx**2
                  self.dy2 = dy**2
-                 self.nx = int(L/dx)
-                 self.ny = int(L/dy)
+                 self.nx = int(round(L/dx))
+                 self.ny = int(round(L/dy))
                 # For stability, this is the largest interval possible
                 # for the size of the time-step:
                  self.dt = self.dx2*self.dy2/( 2*nu*(self.dx2+self.dy2) )
-                # unknown u at new time level t and u at the previous 
-                # time level t-dt
-                 self.ui = self.get_initial_conditions(kind)
+                 self.u = self.get_initial_conditions(kind)
                  
     def get_initial_conditions(self, kind):#how to improve the implementation of IC/vectorization
         """Get the possible initial condition to solve the diffusion function, the options are: 
@@ -37,63 +36,72 @@ class Diffusion(object):
             "rod"
             "semicircle"
         """
-        # Start u and ui off as zero matrices:
-        ui = sp.zeros([self.nx,self.ny])
+        # Start u:
+        u = np.zeros([self.nx,self.ny,self.nt])
         # Now, set the initial conditions (ui).
         for i in range(self.nx):
             for j in range(self.ny):
                 if kind == "circle":
                     p = (i*self.dx - self.L*0.5)**2 + (j*self.dy - self.L*0.5)**2
                     if ( p  <= self.L*0.1 ):
-                        ui[i,j] = 1
+                        u[i,j,0] = 1
                 if kind == "two_circles":
                     p = (i*self.dx - self.L*0.5)**2 + (j*self.dy - self.L*0.25)**2
                     q = (i*self.dx - self.L*0.5)**2 + (j*self.dy - self.L*0.75)**2
                     if ( p  <= self.L*0.03 or q <= self.L*0.03 ):
-                        ui[i,j] = 1 
+                        u[i,j,0] = 1 
                 elif kind == "square":
-                    if ( i>int(0.4*self.nx) and i<int(0.6*self.nx) and j>int(0.4*self.nx) and j<int(0.6*self.nx)):
-                        ui[i,j] = 1
+                    if ( i>int(round(0.4*self.nx)) and i<int(round(0.6*self.nx)) and j>int(round(0.4*self.nx)) and j<int(round(0.6*self.nx))):
+                        u[i,j,0] = 1
                 elif kind == "donut":
                     p = (i*self.dx - self.L*0.5)**2 + (j*self.dy - self.L*0.5)**2
                     if ( p  <= 0.5*self.L and p >= 0.3*self.L ):
-                        ui[i,j] = 1
+                        u[i,j,0] = 1
                 elif kind == "concentric_circles":
                     p = (i*self.dx - self.L*0.5)**2 + (j*self.dy - self.L*0.5)**2
                     if ( p  <= .3*self.L and p >= 0.2*self.L ):
-                        ui[i,j] = 1
+                        u[i,j,0] = 1
                     elif ( p  <= .9*self.L and p >= 0.8*self.L ):
-                        ui[i,j] = 1
+                        u[i,j,0] = 1
                 elif kind == "rod" :
-                    if ( i>int(0.4*self.nx) and i<int(0.45*self.nx) and j>int(0.1*self.ny) and j<int(0.9*self.ny)):
-                        ui[i,j] = 1
+                    if ( i>int(round(0.4*self.nx)) and i<int(round(0.45*self.nx)) and j>int(round(0.1*self.ny)) and j<int(round(0.9*self.ny))):
+                        u[i,j,0] = 1
                 elif kind == "semicircle" :
                     p = (i*self.dx - self.L*0.5)**2 + (j*self.dy - self.L*0.5)**2
                     if ( p  <= self.L*0.2 and j>=int(0.5*self.ny) ):
-                        ui[i,j] = 1      
-        return ui
+                        u[i,j,0] = 1
+                elif kind == "grid":
+                    if ( i>int(round(0.2*self.nx)) and i<int(round(0.3*self.nx)) or i>int(round(0.7*self.nx)) and i<int(round(0.8*self.nx))  or 
+                        j>int(round(0.2*self.ny)) and j<int(round(0.3*self.ny)) or j>int(round(0.7*self.ny)) and j<int(round(0.8*self.ny))):
+                        u[i,j,0] = 1       
+
+        return u
     
     def evolve_ts(self):
-        for i in range(self.nt + 1):#the cicle is nedeed? 
-         self.u[1:-1, 1:-1] = self.ui[1:-1, 1:-1] + self.nu*self.dt*( (self.ui[2:, 1:-1]
-                              - 2*self.ui[1:-1, 1:-1] + self.ui[:-2, 1:-1])/self.dx2 + 
-                              (self.ui[1:-1, 2:] - 2*self.ui[1:-1, 1:-1] + self.ui[1:-1, :-2])/self.dy2 )
-         self.ui = self.u.copy()
+     #   u = np.zeros([self.nx,self.ny,self.nt])
+        for n in range(1, self.nt):       
+            self.u[1:-1, 1:-1, n] = self.u[1:-1, 1:-1, n-1] + self.nu * self.dt * (
+            (self.u[2:, 1:-1, n-1] - 2*self.u[1:-1, 1:-1, n-1] + self.u[:-2, 1:-1,n-1])/self.dx2
+            + (self.u[1:-1, 2: ,n-1] - 2*self.u[1:-1, 1:-1, n-1] + self.u[1:-1, :-2, n-1])/self.dy2 )
+            
          
-         return self.u, self.ui
+        return self.u #, self.ui
         
-
-         
+"""
 #%%               
-Diff1=Diffusion(0.1, 0.1, 1,"semicircle",100,10)     
-ui=Diff1.get_initial_conditions("semicircle")          
-
+Diff1=Diffusion(0.1, 0.1, 1,"grid",1000,10)     
+#ui=Diff1.get_initial_conditions("two_circles")    #da togliere
+Diff1.evolve_ts()
+T=np.zeros([Diff1.nx, Diff1.ny])      
+for i in range (0,Diff1.nx) :
+    for j in range (0,Diff1.ny): 
+        T[i,j]=Diff1.u[i,j,Diff1.nt-1]
+     
 
 plt.figure(figsize=(15, 7))
 
 
 # plt.grid(True, which='major', linestyle='--', color='black', alpha=0.8)
-plt.imshow(ui)
+plt.imshow(T)
 plt.show()
-
-
+"""
