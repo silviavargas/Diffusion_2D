253ce3d6bec338a4813af290b2df49ced75a2ea0
Silvia Vargas <silvia.vargas@studio.unibo.it>
2021-01-08 18:56:17 +0100

    Updated Diff_animation_2D.py

--- Diff_animation_2D.py
+++ Diff_animation_2D.py
@@ -9,31 +9,29 @@ print('numpy: '+np.version.full_version)
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
 print('matplotlib: '+matplotlib.__version__)
 from Diffusion_2D import Diffusion
 
 #%%               
-Diff1=Diffusion(0.1, 0.1, 1,"circle",100 ,10)
-Diff1.evolve_ts()
+Diff1=Diffusion(0.1, 0.1, 1,"donut",100 ,10)
 #%%
 fps = 10 # frame per sec
+Diff1.evolve_ts()
 
-
-
-x = np.linspace(0,Diff1.L, Diff1.nx+1)
-y = np.linspace(0,Diff1.L, Diff1.ny+1)
-x, y = np.meshgrid(x, y)
-
+x = np.linspace(0,Diff1.L, Diff1.nx)
+y = np.linspace(0,Diff1.L, Diff1.ny)
+X, Y = np.meshgrid(x, x)
 
     
 def update_plot(frame_number, zarray, plot):
      plot[0].remove()
-     plot[0] = ax.plot_surface(x, y, zarray[:,:,frame_number], cmap="magma")
+     plot[0] = ax.plot_surface(X, Y, zarray[:,:,frame_number], cmap="magma")
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-plot = [ax.plot_surface(x, y, Diff1.u[:,:,1], color='0.75', rstride=1, cstride=1)]
+plot = [ax.plot_surface(X, Y, Diff1.u[:,:,0], color='0.75', rstride=1, cstride=1)]
 ax.set_zlim(0,1.1)
 ani = animation.FuncAnimation(fig, update_plot, Diff1.nt, fargs=(Diff1.u, plot), interval=1000/fps)
 
